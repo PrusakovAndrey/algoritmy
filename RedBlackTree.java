4996c6e9917ab16820cda5bc5743673ed37dbfe1
PrusakovAndrey <95374012+PrusakovAndrey@users.noreply.github.com>
2023-04-18 21:01:33 +0300

    Add files via upload

---
+++ RedBlackTree.java
@@ -0,0 +1,121 @@
+
+
+public class RedBlackTree {
+
+    private Node root;
+
+    public boolean add(int value){
+        if (root != null){
+            boolean result = addNode(root, value);
+            root = rebalance(root);
+            root.color = Color.BLACK;
+            return result;
+        } else {
+            root = new Node();
+            root.color = Color.BLACK;
+            root.value = value;
+            return true;
+        }
+    }
+
+    private boolean addNode(Node node, int value){
+        if(node.value == value){
+            return false;
+        } else {
+            if(node.value > value){
+                if(node.leftchild != null) {
+                    boolean result = addNode(node.leftchild, value);
+                    node.leftchild = rebalance(node.leftchild);
+                    return result;
+                } else {
+                    node.leftchild = new Node();
+                    node.leftchild.color = Color.RED;
+                    node.leftchild.value = value;
+                    return true;
+                }
+            } else {
+                if(node.rightchild != null){
+                    boolean result = addNode(node.rightchild, value);
+                    node.rightchild = rebalance(node.rightchild);
+                    node.rightchild = rebalance(node.rightchild);
+                    return result;
+                } else {
+                    node.rightchild = new Node();
+                    node.rightchild.color = Color.RED;
+                    node.rightchild.value = value;
+                    return result;
+                }
+            }
+        }
+    }
+
+    private Node rebalance(Node node){
+        Node result = node;
+        boolean needRebalance;
+        do{
+            needRebalance = false;
+            if(result.rightchild != null && result.rightchild.color == Color.RED && 
+                (result.leftchild == null || result.leftchild.color == Color.BLACK)){
+                    needRebalance = true;
+                    result = rightSwap(result);
+                }
+                if(result.leftchild != null && result.leftchild.color == Color.RED &&
+                result.leftchild.leftchild != null && result.leftchild.leftchild.color == Color.RED) {
+                    needRebalance = true;
+                    result = leftSwap(result);
+                }
+                if(result.leftchild != null && result.leftchild.color == Color.RED &&
+                result.rightchild != null && result.rightchild.color == Color.RED) {
+                    needRebalance = true;
+                    colorSwap(result);
+                }
+            }
+        while (needRebalance);
+        return result;
+}
+    
+    private Node rightSwap(Node node){
+        Node righChild = node.rightchild;
+        Node betweenChild = righChild.leftchild;
+        righChild.leftchild = node;
+        node.rightchild = betweenChild;
+        righChild.color = node.color;
+        node.color = Color.RED;
+        return righChild;
+    }
+
+    private Node leftSwap(Node node){
+        Node leftChild = node.leftchild;
+        Node betweenChild = leftChild.rightchild;
+        leftChild.rightchild = node;
+        node.leftchild = betweenChild;
+        leftChild.color = node.color;
+        node.color = Color.RED;
+        return leftChild;
+    }
+
+    private void colorSwap(Node node){
+        node.rightchild.color = Color.BLACK;
+        node.leftchild.color = Color.RED;
+        node.color = Color.RED;
+    }
+
+    private class Node{
+        private int value;
+        private Color color;
+        private Node leftchild;
+        private Node rightchild;
+
+        @Override
+        public String toString(){
+            return "Node+{" +
+                    "value = " + value +
+                    ", color = " + color + 
+                    "}";
+        }
+    }
+
+    private enum Color{
+        RED, BLACK
+    }   
+}
